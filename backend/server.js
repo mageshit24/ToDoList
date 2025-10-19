040478ec1498afe8b4e27ae677f8ff562c054483
mageshit24 <mageshhariramk@gmail.com>
2025-10-19 18:52:12 +0530

    Server Correction

--- backend/server.js
+++ backend/server.js
@@ -109,5 +109,5 @@ app.delete('/todos/:id', async (req, res) => {
 // Server Listener
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log("🚀 Server running on port " + port);
+  console.log("Server running on port " + port);
 });
